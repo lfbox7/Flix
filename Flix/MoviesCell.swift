8fdb6d45d412362863c72eea7d74f228e8a5d174
lfbox7 <lfbox7@yahoo.com>
2019-10-13 19:57:26 -0500

    Initial Commit

---
+++ Flix/MoviesCell.swift
@@ -0,0 +1,30 @@
+//
+//  MoviesCell.swift
+//  Flix
+//
+//  Created by Leonard Box on 10/13/19.
+//  Copyright © 2019 Leonard Box. All rights reserved.
+//
+
+import UIKit
+
+class MoviesCell: UITableViewCell {
+
+    @IBOutlet weak var posterView: UIImageView!
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
